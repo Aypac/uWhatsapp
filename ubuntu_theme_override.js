a08e88fe953fd38ca9b393478214ebc390283d7d
rvollmer <rvollmer@aypac.de>
2015-05-13 08:00:09 +0200

    further tests

--- ubuntu_theme_override.js
+++ ubuntu_theme_override.js
@@ -1,17 +1,34 @@
 function override_styles() {
-    var themeurl = 'https://raw.githubusercontent.com/Aypac/uWhatsapp/master/overwrite.css';
+  var css=`textarea { display:none; } #wrapper::before { background-color: #00F; } .app-wrapper::before { background-color: #00F; } #window { width: 100%; margin: 0px; }
+    @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (orientation:portrait) {
+    body {
+            -webkit-transform: rotate(90deg);
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+            position: absolute;
+            top: 0;
+            left: 0;
+    }}
+    `;
+
     var cssId = 'devicetheme';
-    if (!document.getElementById(cssId)) {
         var head  = document.getElementsByTagName('head')[0];
-        var link  = document.createElement('link');
+        var link  = document.createElement('style');
         link.id   = cssId;
         link.rel  = 'stylesheet';
         link.type = 'text/css';
-        link.href = themeurl;
+        //link.href = themeurl;
+        link.innerHTML = css;
         link.media = 'all';
         head.appendChild(link);
+
+    var viewport  = document.createElement('meta');
+    viewport.name="viewport";
+    viewport.content="initial-scale=0.65, maximum-scale=0.65";
+    head.appendChild(viewport);
+
         alert('Willkommen auf dem UbuntuPhone');
-    }
 }
 window.addEventListener('load', override_styles, true);
 
