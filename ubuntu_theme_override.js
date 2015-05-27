4695e8c0105738ab23d732fcf1409d75d2858a5e
rvollmer <rvollmer@aypac.de>
2015-05-28 00:57:35 +0200

    +Switching between Contact List and Chats +-En-/Disabling Text field (this needs some more fixing) -Portrait not working

--- ubuntu_theme_override.js
+++ ubuntu_theme_override.js
@@ -1,18 +1,40 @@
+/*
+
+  UbuntuPhoneBQ: width=540, height=919
+
+
+    @media screen and (orientation:portrait) {
+      body {
+        -ms-transform: rotate(-90deg); // IE 9
+        -webkit-transform: rotate(-90deg); // Chrome, Safari, Opera
+        transform: rotate(-90deg);
+            transform: translate(0,0);
+        width: 100%; // screen width
+        height: 100%;// screen height
+        overflow: scroll;
+      }
+    }
+    #wrapper::before { background-color: #00F; } .app-wrapper::before { background-color: #00F; }
+    textarea { display:none; }
+    #window { width: 100%; height: 100%; padding:0px; margin: 0px; }
+*/
+
 function override_styles() {
-  var css=`textarea { display:none; } #wrapper::before { background-color: #00F; } .app-wrapper::before { background-color: #00F; } #window { width: 100%; margin: 0px; }
-    @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (orientation:portrait) {
-    body {
-            -webkit-transform: rotate(90deg);
-            width: 100%;
-            height: 100%;
-            overflow: hidden;
-            position: absolute;
-            top: 0;
-            left: 0;
-    }}
-    `;
+
+    //inject css
 
     var cssId = 'devicetheme';
+        if (document.getElementById(cssId)===null) {
+    var css=`
+    .pane-chat { width: 100%; position: absolute; left: 0px; } .pane-list { width: 100%; } #main { z-index:90; display: float; }
+    .input::after { contenteditable: false; }
+    #leiste { background-color: #fff; border: 1pt solid #000; border-radius: 15px; color: #888; font-size: 30pt; height: 50px; width: 50px;
+         position: fixed; top: 200px; left: 20px; z-index: 999;
+         text-align: center; vertical-align: center;
+    }
+    .textactivate::after { font-size:20pt; padding-left:0pt; margin-left:4pt; margin-bottom:4pt; color:#777; }
+    `;
+
         var head  = document.getElementsByTagName('head')[0];
         var link  = document.createElement('style');
         link.id   = cssId;
@@ -23,12 +45,125 @@ function override_styles() {
         link.media = 'all';
         head.appendChild(link);
 
+            //Scale down the view
         var viewport  = document.createElement('meta');
         viewport.name="viewport";
         viewport.content="initial-scale=0.65, maximum-scale=0.65";
         head.appendChild(viewport);
+    }
 
-        alert('Welcome to Whatsapp on UbuntuPhone!');
+        var intros=document.getElementsByClassName('pane-intro');
+        if (intros.length>0) {
+            var intro = intros[0];
+            intro.setAttribute('style','display:none; width:0%;');
+        }
+
+    //Create a small icon to toggle Sidebar
+    if (document.getElementById('leiste')===null) {
+        showSidebar();
+        var node = document.createElement("div");
+        node.id="leiste";
+        node.innerHTML="⇔";
+        node.onclick=toggleSidebar;
+        node.onClick=toggleSidebar;
+        var global=document.body;
+        global.appendChild(node);
+    }
+
+
+    //Disable Text fields
+    disableAllText();
+
 }
+
+
+
+function toggleSidebar() {
+    console.log('toggle sidebar');
+    if (document.getElementById('main').style.display != "none") {
+        showSidebar();
+    } else {
+        hideSidebar();
+    }
+}
+
+function hideSidebar() {
+    console.log('hide sidebar');
+    //Hide the sidebar
+    var side=document.getElementById('side');
+    if (side!=null) {
+        side.setAttribute('style',"display:none;");// width:0%;
+    }
+    var main = document.getElementById('main');
+    if (main!=null) {
+        main.setAttribute('style',"display:float;"); //width:100%; height: 100%; overflow: scroll;
+    }
+    disableAllText();
+}
+
+
+function showSidebar() {
+    console.log('show sidebar');
+    //Show the sidebar
+    var side=document.getElementById('side');
+    console.log(side);
+    if (side!=null) {
+        console.log('sidebar found. show');
+        side.setAttribute('style',"display:float;");
+    }
+    var main = document.getElementById('main');
+    console.log(main);
+    if (main!=null) {
+        console.log('mainbar found. hide');
+        main.setAttribute('style',"display:none;");
+    }
+    disableAllText();
+}
+
+function disableAllText() {
+    var inputs=document.getElementsByClassName('block-compose');
+    for (var i=0;i<inputs.length;i++) {
+        var input=inputs[i];
+        var field=input.getElementsByClassName('input')[0];
+
+
+            //Add button
+            var buts=input.getElementsByClassName('ptt-container');
+            var textact=input.getElementsByClassName('textactivate');
+            if (buts.length <= 1 && textact.length == 0) {
+
+                if (field.contentEditable!="false") {
+                    field.contentEditable='false';
+                }
+
+                console.log('add textfield button');
+                var textact = document.createElement("div");
+                textact.innerHTML="⌨";
+                textact.setAttribute('class',"ptt-container textactivate");
+                textact.onclick=toggleText;
+                textact.onClick=toggleText;
+                input.appendChild(textact);
+            }
+    }
+}
+
+function toggleText(event) {
+    var textact = event.target;
+    console.log(textact);
+
+    input=textact.parentElement.parentElement.parentElement;
+    var field=input.getElementsByClassName('input')[0];
+
+    if (field.contentEditable!="false") {
+        field.contentEditable='false';
+    } else {
+        field.contentEditable='true';
+    }
+}
+
+override_styles();
+
+setInterval(disableAllText,2000);
+
 window.addEventListener('load', override_styles, true);
 
